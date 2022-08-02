753055910cd0c68df8165567cf0ce3c59010c778
NattapatSaipun <107970974+NattapatSaipun@users.noreply.github.com>
2022-08-03 00:13:16 +0700

    commit date 3-Aug-22

--- App.js
+++ App.js
@@ -1,47 +1,17 @@
-import { View, Text ,StyleSheet} from 'react-native'
-import React from 'react'
-import Logo from './components/Logo'
-import User from './components/User'
-import LotsOfGreetings from './components/LotsOfGreetings'
-import MyCustomTextWith from './components/MyCustomTextWith'
-import Count from './components/Count'
-import InputText from './components/InputText'
-import UserNameandPassWord from './components/UserNameandPassWord'
-import UserNamePassword from './components/UserNamePassword'
-import AlertComponent from './components/AlertComponent'
-import ImageWithTextInput from './components/ImageWithTextInput'
-import ButtonExample from './components/ButtonExample'
-import Touchable_Example from './components/Touchable_Example'
-import TouchablePractice from './components/TouchablePractice'
-import DynamicStyle from './components/DynamicStyle'
-import ModelExample from './components/ModelExample'
-const App = () => {
-  return (
-    <View style={styles.container}>
-      {/* <Logo />
-      <User /> */}
-      {/* <LotsOfGreetings/> */}
-      {/* <MyCustomTextWith/> */}
-      {/* <Count/> */}
-      {/* <InputText/> */}
-      {/* <UserNameandPassWord/> */}
-      {/* <UserNamePassword/> */}
-      {/* <AlertComponent/> */}
-      {/* <ImageWithTextInput/> */}
-      {/* <ButtonExample/>  */}
-      {/* <Touchable_Example/>   */}
-      {/* <TouchablePractice/>  */}
-      {/* <DynamicStyle/>  */}
-      <ModelExample/>
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import ModalPractice from './components/ModalPractice';
 
+export default function App() {
+  return (
+    <View>
+      <ModalPractice/>
     </View>
   )
+   
+  
 }
 
-export default App
-
 const styles = StyleSheet.create({
-  container:{
-    flex: 1, alignItems: 'center', justifyContent: 'center'
-  }
-})
+  
+});
